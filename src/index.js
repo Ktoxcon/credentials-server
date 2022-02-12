99e2f296d2ffc806308a112704f93d1ddb69ec38
Kenneth Toxcon <kennethtoxro@gmail.com>
2022-02-11 21:37:05 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const validateRole = require("./role-validator");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/credentials/api", (req, res) => {
+  const { role } = req.body;
+
+  if (!role) {
+    res.status(401).send({
+      email: "",
+      password: "",
+    });
+    res.end();
+  }
+
+  const isValidRole = validateRole(role);
+
+  if (!isValidRole) {
+    res.status(401).send({ email: "", password: "" });
+    res.end();
+  } else {
+    res.send({ email: "sus", password: "sus" });
+  }
+});
+
+app.listen(80, () => {
+  console.log("Server Running");
+});
